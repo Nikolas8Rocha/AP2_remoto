6f142b8bd4b957ee41dc99aee7875817a64ef919
Nikolas <nikolasrm@al.insper.edu.br>
2024-10-19 18:41:44 -0300

    definindo a função preenche_frota

--- funcoes.py
+++ funcoes.py
@@ -14,3 +14,35 @@ def define_posicoes (linha, coluna, orientacao, tamanho):
             posicoes_ocupadas.append ([linha_2, coluna])
             i+=1
     return posicoes_ocupadas
+
+
+
+def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
+    for navios in frota.keys():
+        if navios not in frota:
+            frota[navios] = define_posicoes(linha, coluna, orientacao, tamanho)
+        else:
+            a = frota [nome]
+            b = [define_posicoes(linha, coluna, orientacao, tamanho)]
+            frota[nome] = [a,b]
+            #frota[navios] += define_posicoes(linha, coluna, orientacao, tamanho)
+        
+    
+    return frota
+
+
+
+frota = {
+  "navio-tanque":[
+    [[6,1],[6,2],[6,3]]
+  ]
+}
+nome_navio = 'navio-tanque'
+linha = 4
+coluna = 7
+orientacao = 'vertical'
+tamanho = 3
+
+resultado = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+print(resultado)
+
