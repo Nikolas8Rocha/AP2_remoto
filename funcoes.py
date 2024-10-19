883bed5b78ba34bc7535fd7014347326d5ecad45
Pedro Kramer <pedroaverame@gmail.com>
2024-10-19 18:44:23 -0300

    Erro funcao preenche_frota corrigido

--- funcoes.py
+++ funcoes.py
@@ -32,17 +32,3 @@ def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
 
 
 
-frota = {
-  "navio-tanque":[
-    [[6,1],[6,2],[6,3]]
-  ]
-}
-nome_navio = 'navio-tanque'
-linha = 4
-coluna = 7
-orientacao = 'vertical'
-tamanho = 3
-
-resultado = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
-print(resultado)
-
