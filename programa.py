18a8f4b7a4148cad3b67c1ddf5b84493fac4392d
Nikolas <nikolasrm@al.insper.edu.br>
2024-10-24 08:20:20 -0300

    primeiro programa otimizado

--- programa.py
+++ programa.py
@@ -1,9 +1,5 @@
 import funcoes
 
-
-
-i = 0
-
 frota = {
     "porta-aviões":[],
     "navio-tanque":[],
@@ -11,97 +7,48 @@ frota = {
     "submarino": [],
 }  
 
-while i < 10:
+navios = [
+    ['porta-aviões',4],
+    ['navio-tanque',3],
+    ['navio-tanque',3],
+    ['contratorpedeiro',2],
+    ['contratorpedeiro',2],
+    ['contratorpedeiro',2],
+    ['submarino',1],
+    ['submarino',1],
+    ['submarino',1],
+    ['submarino',1],
+]
+for barco in navios:
+    navio = barco[0]
+    tamanho = barco[1]
+    print(f"Insira as informações referentes ao navio {navio} que possui tamanho {tamanho}")
+    linha = int(input("Linha: "))
+    coluna = int(input("Coluna: "))
+    if tamanho == 1:
+        orientacao = 'vertical'
+    else:
+        orientacao = int(input("[1] Vertical [2] Horizontal "))
+        if orientacao == 1:
+            orientacao = "vertical"
+        else:
+            orientacao = "horizontal"
 
-    if i > 5:
-        print("Insira as informações referentes ao navio submarino que possui tamanho 1")
+    valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, tamanho)
+    while valida == False:
+        print("Esta posição não está válida!")
+        print(f"Insira as informações referentes ao navio {navio} que possui tamanho {tamanho}")
         linha = int(input("Linha: "))
         coluna = int(input("Coluna: "))
-
-            
-        valida = funcoes.posicao_valida(frota, linha , coluna,"vertical", 1)
-        while valida == False:
-            print("Esta posição não está válida!")
-            print("Insira as informações referentes ao navio submarino que possui tamanho 1")
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
-
-            valida = funcoes.posicao_valida(frota, linha , coluna, "vertical", 1)
-        frota = funcoes.preenche_frota(frota,"submarino",linha, coluna, orientacao, 1)
-    
-    else:
-        if i == 0:
-            print("Insira as informações referentes ao navio porta-aviões que possui tamanho 4")
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
+        if tamanho == 1:
+            orientacao = 'vertical'
+        else:
             orientacao = int(input("[1] Vertical [2] Horizontal "))
             if orientacao == 1:
                 orientacao = "vertical"
             else:
                 orientacao = "horizontal"
-            
-            valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, 4)
-            while valida == False:
-                print("Esta posição não está válida!")
-                print("Insira as informações referentes ao navio porta-aviões que possui tamanho 4")
-                linha = int(input("Linha: "))
-                coluna = int(input("Coluna: "))
-                orientacao = int(input("[1] Vertical [2] Horizontal "))
-                if orientacao == 1:
-                    orientacao = "vertical"
-                else:
-                    orientacao = "horizontal"
-                valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, 4)
-            frota = funcoes.preenche_frota(frota,"porta-aviões",linha, coluna, orientacao, 4)
-        elif i > 0 and i <= 2:
-            print("Insira as informações referentes ao navio navio-tanque que possui tamanho 3")
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
-            orientacao = int(input("[1] Vertical [2] Horizontal "))
-            if orientacao == 1:
-                orientacao = "vertical"
-            else:
-                orientacao = "horizontal"
-            
-            valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, 3)
-            while valida == False:
-                print("Esta posição não está válida!")
-                print("Insira as informações referentes ao navio navio-tanque que possui tamanho 3")
-                linha = int(input("Linha: "))
-                coluna = int(input("Coluna: "))
-                orientacao = int(input("[1] Vertical [2] Horizontal "))
-                if orientacao == 1:
-                    orientacao = "vertical"
-                else:
-                    orientacao = "horizontal"
-                valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, 3)
-            frota = funcoes.preenche_frota(frota,"navio-tanque",linha, coluna, orientacao, 3)
-        elif i > 2 and i <= 5:            
-            print("Insira as informações referentes ao navio contratorpedeiro que possui tamanho 2")
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
-            orientacao = int(input("[1] Vertical [2] Horizontal "))
-            if orientacao == 1:
-                orientacao = "vertical"
-            else:
-                orientacao = "horizontal"
-            
-            valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, 2)
-            while valida == False:
-                print("Esta posição não está válida!")
-                print("Insira as informações referentes ao navio contratorpedeiro que possui tamanho 2")
-                linha = int(input("Linha: "))
-                coluna = int(input("Coluna: "))
-                orientacao = int(input("[1] Vertical [2] Horizontal "))
-                if orientacao == 1:
-                    orientacao = "vertical"
-                else:
-                    orientacao = "horizontal"
-                valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, 2)
-            frota = funcoes.preenche_frota(frota,"contratorpedeiro",linha, coluna, orientacao, 2)
-
-    i += 1
-
+        valida = funcoes.posicao_valida(frota, linha , coluna, orientacao, tamanho)
+    frota = funcoes.preenche_frota(frota,navio,linha, coluna, orientacao, tamanho)
 
 print(frota)
-
